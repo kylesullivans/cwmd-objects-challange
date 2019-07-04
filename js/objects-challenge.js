2923ea38a2b8a159c154ecbbd84100aee80a5cf0
Kyle Sullivan <kyledanielsullivan@gmail.com>
2019-07-04 00:11:52 -0400

    Add files via upload

---
+++ js/objects-challenge.js
@@ -0,0 +1,38 @@
+/* 
+    Name:
+    Student ID:
+    Date:
+    Desc:
+*/
+
+// For all Console Logs use String Literals 
+
+// 1. Create a new Boolean named isFridgeRuning set to either true or false and display the value in the console.
+
+ 
+// 2. Attach a property to isFridgeOn name caughtIt and set to either true or false. Display it in the console
+
+ 
+// 3. Create a new Number as an object and display it in the console.
+    
+  
+
+// 4. Attach 2 properties to your number object, one named oneLess and one name oneMore. Display them in the console.
+
+    
+
+// 5. Create a new String. Display it in the console.
+
+   
+
+// 6. Use the split function to display each character in the console.
+    
+    
+
+// 7. Create a new Date object with the release date of you favourite movie. 
+
+    
+
+// 8. Display the date in the console as a string, a date string and a locale date string
+   
+
